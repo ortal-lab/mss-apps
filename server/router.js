40f8656c4a8ade6c16cd07b6fcd7ffbf447388fb
ortal-lab <79470883+ortal-lab@users.noreply.github.com>
2022-11-30 22:34:05 +0200

    updated server

--- server/router.js
+++ server/router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const {paginatedResults}= require('./logic')
 router.get('/',(req, res) => {
-    res.send(req.images);
+    res.send(req.images.slice(0,9));
   })
 router.get("/paginate", paginatedResults(), (req, res) => {
   res.json(res.paginatedResults);
